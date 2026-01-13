18fac313a77c5d1f9c2d8e8c1f521f18eb1e279a
Aadil Ansari <notaadilansari@gmail.com>
2026-01-13 17:29:15 +0530

    Change API key in Temp.py
    Updated API key for OpenWeatherMap.

--- Temp.py
+++ Temp.py
@@ -1,7 +1,7 @@
 import json
 import requests
 city_name=input("Enter a city name : ")
-api_key="6ee2873eaaec145c9adc521f361e7f6d"
+api_key="6ee2873eaaec145c9adc521f361e7f6l"
 api_url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
 get_server_info=requests.get(api_url)
 data =get_server_info.json()
