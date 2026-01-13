0a7ed53f7a7e211ad649c8ea7a5920f6c43d575d
Aadil Ansari <notaadilansari@gmail.com>
2026-01-13 17:35:47 +0530

    Enhance output message for temperature display

--- Temp.py
+++ Temp.py
@@ -1,9 +1,10 @@
 import json
 import requests
+print("🌦️Welcome to get temp by city name program ")
 city_name=input("Enter a city name : ")
 api_key="6ee2873eaaec145c9adc521f361e7f6l"
 api_url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
 get_server_info=requests.get(api_url)
 data =get_server_info.json()
 temp=data["main"]["temp"]
-print(f"temp of {city_name} is {temp}")
+print(f"☀️Temp of {city_name} is : {temp} ")
